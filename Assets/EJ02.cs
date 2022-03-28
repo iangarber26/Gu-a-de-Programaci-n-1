eb14f26bfb7f5d576211ca39fffb32aa0476e8c7
iangarber26 <88200710+iangarber26@users.noreply.github.com>
2022-03-28 12:12:39 -0300

    up to ex 5 completed

---
+++ Assets/EJ02.cs
@@ -0,0 +1,29 @@
+﻿//2. Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,  respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+//a. Suma 
+//b. Producto 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ02 : MonoBehaviour
+{
+    int num1 = 4;
+    int num2 = 5;
+    int suma;
+    int producto;
+
+    void Start()
+    {
+        suma = num1 + num2;
+        Debug.Log("El resultado de sumar 4 y 5 es " + suma);
+        producto = num1 * num2;
+        Debug.Log("El producto resultante es " + producto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
